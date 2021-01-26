138433bf8840b58e02cae160698140f9d2b22883
cTraymany <chindalatht@gmail.com>
2021-01-26 10:39:46 -0800

    coding challenge 2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,19 +1,19 @@
-const markWeight = 78
-const markHeight = 1.69
-const johnWeight = 92
-const johnHeight = 1.95
+// const markWeight = 78
+// const markHeight = 1.69
+// const johnWeight = 92
+// const johnHeight = 1.95
 
-const markBMI = markWeight / markHeight**2
-const johnBMI = johnWeight / johnHeight**2
+// const markBMI = markWeight / markHeight**2
+// const johnBMI = johnWeight / johnHeight**2
 
-const markHigherBMI = markBMI > johnBMI
+// const markHigherBMI = markBMI > johnBMI
 
-// console.log(markBMI, johnBMI, markHigherBMI)
+// // console.log(markBMI, johnBMI, markHigherBMI)
 
-if (markBMI > johnBMI) {
-    console.log(`Mark's BMI(${markBMI}) is higher than John's(${johnBMI})!`)
-} else if (johnBMI > markBMI) {
-    console.log(`John's BMI(${johnBMI}) is higher than Mark's(${markBMI})!`)
-} else if (johnBMI === markBMI) {
-    console.log("Same BMI!")
-}
+// if (markBMI > johnBMI) {
+//     console.log(`Mark's BMI(${markBMI}) is higher than John's(${johnBMI})!`)
+// } else if (johnBMI > markBMI) {
+//     console.log(`John's BMI(${johnBMI}) is higher than Mark's(${markBMI})!`)
+// } else if (johnBMI === markBMI) {
+//     console.log("Same BMI!")
+// }
---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,11 @@
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+];
+
+for (let i = 0; i < jonasArray.length; i++) {
+    console.log(jonasArray[i])
+}
