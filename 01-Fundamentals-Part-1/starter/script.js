7563956bfc8a1647a3b3e044a702bd2f1257ad79
cTraymany <chindalatht@gmail.com>
2021-01-26 10:08:51 -0800

    coding challenge 2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,12 +1,19 @@
-/*
-Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+const markWeight = 78
+const markHeight = 1.69
+const johnWeight = 92
+const johnHeight = 1.95
 
-1. Store Mark's and John's mass and height in variables
-2. Calculate both their BMIs using the formula (you can even implement both versions)
-3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+const markBMI = markWeight / markHeight**2
+const johnBMI = johnWeight / johnHeight**2
 
-TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
-TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+const markHigherBMI = markBMI > johnBMI
 
-GOOD LUCK 😀
-*/
+// console.log(markBMI, johnBMI, markHigherBMI)
+
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI(${markBMI}) is higher than John's(${johnBMI})!`)
+} else if (johnBMI > markBMI) {
+    console.log(`John's BMI(${johnBMI}) is higher than Mark's(${markBMI})!`)
+} else if (johnBMI === markBMI) {
+    console.log("Same BMI!")
+}
